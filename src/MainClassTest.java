cdceed3ece9bba54612d9308cf0c9141ba1ea0dd
ferdoznik <64578728+ferdoznik@users.noreply.github.com>
2023-02-25 20:58:01 +0300

    Test 1

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -8,7 +8,7 @@ public class MainClassTest extends MainClass
     {
         int result = a;
         if (result !=14){
-            Assert.fail("Didn't return 14");
+            Assert.fail("Didn't get 14");
         }
 }
 }
